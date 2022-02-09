edb4c873df904af7909f8b0ffddb78359958c6af
kbylander <98012682+kbylander@users.noreply.github.com>
2022-02-09 12:30:30 +0100

    Login form and performlogin
    Login form and unfinished performlogin, as well as a folder structure for login/logout

---
+++ Login/login.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="style_reg.css">
+</head>
+<body>
+
+<form action="performlogin.php" method='POST'>
+
+<div>
+<h1>Login form</h1>
+<p>Log in with your credentials:</p>
+<hr>
+
+<label><b>Email:</b></label>
+<input type="text" placeholder="Enter Email" name="useremail" ><br><br>
+
+<label><b>Password:</b></label>
+<input type="text" placeholder="Enter Password" name="userpassword" ><br><br> <!--change to type="password" later-->
+
+<button type="submit">Register</button>
+<div><button type="button">Cancel</button>
+
+</div></div>
+</form>
+</body>
+</html>
