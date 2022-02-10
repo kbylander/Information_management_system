54d44f51a6eb13049816e56319b20e869d05753f
cabo8003 <carl.bogstedt.8003@student.uu.se>
2022-02-10 16:09:57 +0100

    Daredevil measurement (working)

--- Algorithms/Calle_test.php
+++
@@ -1,8 +0,0 @@
-<?php
-        $fasta1 = $_GET['fastafile1'];
-        $fasta2 = $_GET['fastafile2'];
-
-        $output = shell_exec("Rscript function_call.R $fasta1 $fasta2");
-        echo $output
-  
-    ?> 
