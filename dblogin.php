716b285839a9036ab2120c128d39cc44fa927cc6
pcr08 <52033249+pcr08@users.noreply.github.com>
2022-02-10 15:00:55 +0100

    p1

---
+++ dblogin.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Database</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<section class="header">
+        <nav>
+            <div class ="nav-links">
+                <ul>
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="About.php">About</a></li>
+                    <li><a href="#">Database</a></li>
+                    <li><a href="ContactUs.php">Contact Us</a></li>
+                </ul>
+            </div>
+        </nav>
+    <div class="LanguageToggle">
+                    <div class="GoogleTranslate">
+                        <div id="google_translate_element" style="text:right;"></div><script type="text/javascript">
+                        function googleTranslateElementInit() {
+                          new google.translate.TranslateElement({pageLanguage: 'en', includedLanguages: 'ar,de,en,es,it,ja,pt,ru,zh-CN,zh-TW', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+                        }
+                        </script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
+                    </div>
+    </div>
+</section>
+<footer>
+    <p>©Copyright 2022 by Daredevils. All rights reversed.</p>
+</footer>
+</body>
+</html>
