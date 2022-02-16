cb06fc28de5896bf6efbe5c411f6729fb6ed2f5d
cdallas8 <92080785+cdallas8@users.noreply.github.com>
2022-02-16 17:50:46 +0100

    Update function_form.php

--- Algorithms/function_form.php
+++ Algorithms/function_form.php
@@ -7,6 +7,7 @@
 <form action="upload.php" method="POST">
     <input type="file" name = "fastafile1"> 
     <input type="file" name = "fastafile2"> 
+    <! --- drop down menu to chose which measurement method to use or consensus --->
     <input type="submit" name="submit">
 </form>
 
