7a892f2d71eb42ec8aee62b36a0021db01ee6e3b
lbrok <swedishnrg@gmail.com>
2022-02-21 16:23:20 +0100

    Pages to view sequences and individuals in database

--- Database/individual.php
+++ Database/individual.php
@@ -1,4 +1,4 @@
-<?PHP
+<?php
 session_start();
 
 //Check so the user is logged in, and has access to the database
@@ -8,11 +8,9 @@ if(!isset($_SESSION['loggedin'])) {
 if ($_SESSION['loggedin'] == False) {
   header('Location: ../index.php');
 }
-//If the user is logged in they will have access to view all sequences
-//We therefor create a query to get the data from the database
 else{
-$userID = $_SESSION['user'];
-$query = "SELECT entryID, gender, species, date FROM entries WHERE addedby LIKE '$userID'";
+$individualID = $_GET['ID'];
+$query = "SELECT seqID, genename, seq FROM sequence WHERE entryID LIKE '$individualID'";
 include '../connectDB.php';
 $result = mysqli_query($link, $query);
 include '../disconnectDB';
@@ -22,29 +20,27 @@ include '../disconnectDB';
 <!DOCTYPE html>
 <html>
 <body>
-  <h1>Individuals in database</h1>
-  <input type="text" id="searchInput" onkeyup="searchFunction()" placeholder="Search in database">
+  <h1>Sequences uploaded for <?php echo $individualID ?></h1>
+  <input type="text" id="searchInput" onkeyup="searchFunction()" placeholder="Search among sequences">
     <table id="entryTable">
         <tr>
             <th>ID</th>
-            <th>Gender</th>
-            <th>Species</th>
-            <th>Date added</th>
+            <th>Gene</th>
+            <th>Sequence</th>
         </tr>
 
 <!-- populate table from mysql database -->
     <?php while($row = mysqli_fetch_array($result)):?>
       <tr>
-        <td><a href="individual.php?ID=<?php echo $row[0] ?>"><?php echo $row[0];?></td>
+        <td><?php echo $row[0];?></td>
         <td><?php echo $row[1];?></td>
         <td><?php echo $row[2];?></td>
-        <td><?php echo $row[3];?></td>
       </tr>
         <?php endwhile;?>
       </table>
 
 
-<button onclick="window.location.href='Entryhandling/insertDB.php'">New entry</button>
+<button onclick="window.location.href='database.php'">BACK</button>
 
 <!-- Script to real time filter the able -->
 <script>
