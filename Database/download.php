6949a55816e0c3239de1559fc89fe4cf135829ce
lbrok <swedishnrg@gmail.com>
2022-02-25 09:52:57 +0100

    Multiple sequence fasta download supported

--- Database/download.php
+++ Database/download.php
@@ -1,35 +1,76 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 session_start();
 if(isset($_GET['file']) && $_SESSION['loggedin']){
-$seqID = $_GET['file'];
+  $seqID = $_GET['file'];
 
-$query = "SELECT sequence.genename, sequence.seq, entries.species FROM sequence LEFT JOIN entries ON sequence.entryID = entries.entryID WHERE seqID LIKE '$seqID'";
-include '../connectDB.php';
-$result = mysqli_query($link, $query);
-include '../disconnectDB';
+  $query = "SELECT sequence.genename, sequence.seq, entries.species FROM sequence LEFT JOIN entries ON sequence.entryID = entries.entryID WHERE seqID LIKE '$seqID'";
+  include '../connectDB.php';
+  $result = mysqli_query($link, $query);
+  include '../disconnectDB.php';
 
-while($row = mysqli_fetch_array($result)):
-$gene = $row[0];
-$seq = $row[1];
-$species = $row[2];
-endwhile;
+  while($row = mysqli_fetch_array($result)):
+  $gene = $row[0];
+  $seq = $row[1];
+  $species = $row[2];
+  endwhile;
 
-require 'fastafilegenerator.php';
-$file = fastagensingle($seqID,$seq,$gene,$species);
+  require 'fastafilegenerator.php';
+  $file = fastagensingle($seqID,$seq,$gene,$species);
 
-ignore_user_abort(true);
-header('Content-Description: File Transfer');
-header('Content-Disposition: attachment; filename='.basename($file));
-header('Expires: 0');
-header('Cache-Control: must-revalidate');
-header('Pragma: public');
-header('Content-Length: ' . filesize("TempFastaFiles/" . $file));
-header("Content-Type: text/plain");
-readfile("TempFastaFiles/" . $file);
+  ignore_user_abort(true);
+  header('Content-Description: File Transfer');
+  header('Content-Disposition: attachment; filename='.basename($file));
+  header('Expires: 0');
+  header('Cache-Control: must-revalidate');
+  header('Pragma: public');
+  header('Content-Length: ' . filesize("TempFastaFiles/" . $file));
+  header("Content-Type: text/plain");
+  readfile("TempFastaFiles/" . $file);
 
-unlink("TempFastaFiles/" . $file);
+  unlink("TempFastaFiles/" . $file);
+}
+elseif(isset($_POST['submit_multiple'])){
+
+    $selected = $_POST['selected'];
+    $seqIDs = "('".join("','",$selected)."')";
+    $query = "SELECT sequence.genename, sequence.seq, entries.species FROM sequence LEFT JOIN entries ON sequence.entryID = entries.entryID WHERE seqID IN $seqIDs";
+    include '../connectDB.php';
+    $result = mysqli_query($link, $query);
+    include '../disconnectDB.php';
+
+    $genes = array();
+    $seqs = array();
+    $species = array();
+
+    while($row = mysqli_fetch_array($result)):
+      array_push($genes, $row[0]);
+      array_push($seqs, $row[1]);
+      array_push($species, $row[2]);
+    endwhile;
+
+    $seqIDs = explode(",",$seqIDs);
+    $seqIDs = str_replace(array('(',')',"'"), '',$seqIDs);
+    require 'fastafilegenerator.php';
+    $file = fastamultiplegen($seqIDs,$seqs,$genes,$species);
+
+    ignore_user_abort(true);
+    header('Content-Description: File Transfer');
+    header('Content-Disposition: attachment; filename='.basename($file));
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize("TempFastaFiles/" . $file));
+    header("Content-Type: text/plain");
+    readfile("TempFastaFiles/" . $file);
+
+    unlink("TempFastaFiles/" . $file);
+
 }
 else{
-header('Location: ../index.php');
+  header('Location: ../index.php');
 }
 ?>
--- Database/fastafilegenerator.php
+++ Database/fastafilegenerator.php
@@ -1,9 +1,20 @@
 <?php
 function fastagensingle($ID, $seq, $gene, $species){
-$filename = $ID . "_fasta.fasta";
-$file = fopen("TempFastaFiles/" . $filename, "w");
-fwrite($file,">GM|" . $ID . "| gene=" . $gene . " os=" . $species . "\n" . $seq);
-fclose($file);
-return ($filename);
+  $filename = $ID . "_fasta.fasta";
+  $file = fopen("TempFastaFiles/" . $filename, "w");
+  fwrite($file,">GM|" . $ID . "| gene=" . $gene . " os=" . $species . "\n" . $seq);
+  fclose($file);
+  return ($filename);
+}
+
+function fastamultiplegen($IDs,$seqs,$genes,$species){
+  $filename = $IDs[0]."_am_fasta.fasta";
+  $file = fopen("TempFastaFiles/" . $filename, "w");
+
+  for ($i = 0; $i < count($seqs); $i++)  {
+    fwrite($file,">GM|" . $IDs[$i] . "| gene=" . $genes[$i] . " os=" . $species[$i] . "\n" . $seqs[$i] . "\n\n");
+  }
+  fclose($file);
+  return ($filename);
 }
 ?>
