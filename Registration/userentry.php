f6ebe3dc1475ab2a0df42a112d81e6eb014cb31d
pcr08 <52033249+pcr08@users.noreply.github.com>
2022-02-28 17:10:07 +0100

    email verification

--- Registration/userentry.php
+++ Registration/userentry.php
@@ -1,5 +1,5 @@
 <?php
-$sql_stmt = "INSERT INTO users (username, hash, email, admin) VALUES ('$username', '$hash', '$email', 0)";
+$sql_stmt = "INSERT INTO users (username, hash, email, admin, active) VALUES ('$username', '$hash', '$email', 0, false)";
 include '../connectDB.php';
 mysqli_query($link,$sql_stmt);
 include '../disconnectDB.php';
--- connectDB.php
+++ connectDB.php
@@ -1,6 +1,6 @@
 <?php
 $user = 'root';
-$password = 'root';
+$password = '';
 $db = 'geneticmatch';
 $host = 'localhost';
 $port = 3306;
