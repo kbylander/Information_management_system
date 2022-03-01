d4111c22481d8746fb2bfc3c1875d3110d6b0e22
kbylander <98012682+kbylander@users.noreply.github.com>
2022-03-01 12:01:16 +0100

    bug fix, login
    FIxed the inlogg bug where as long as the password was long enough it will work

--- Login/conflogin.php
+++ Login/conflogin.php
@@ -8,13 +8,12 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$password = $_POST['userpassword'];
+$passw = $_POST['userpassword'];
 $user = $_POST['username'];
 $userID=$user;
-$passwlen = strlen($password);
+$passwlen = strlen($passw);
 
 if($passwlen > 7) {
-    echo 'password is long enough';
 
     include '../connectDB.php';
     $query = "SELECT hash FROM users WHERE username LIKE '$userID'";
@@ -25,23 +24,21 @@ if($passwlen > 7) {
 
     if ($number_of_entries != 0){
 
-    while($row = mysqli_fetch_array($result)) {
+        while($row = mysqli_fetch_array($result)) {
 
 
-        $hash=$row['hash'];
-        echo $hash;
-    }
-    if (password_verify($password, $hash)) {
-        $_SESSION['user']=$userID;
-        $_SESSION['loggedin'] = TRUE;
-        header('Location:../index.php');
-
-
-        } else {
-            $_SESSION['LoginError'] = $_SESSION['LoginError'] . 'Login failed' . '<br>';
-            header("location:login.php");
-
+            $hash=$row['hash'];
         }
+
+        if (password_verify($passw, $hash)) {
+            $_SESSION['user']=$userID;
+            $_SESSION['loggedin'] = TRUE;
+            header('Location:../index.php');
+        } 
+        else {
+                $_SESSION['LoginError'] = $_SESSION['LoginError'] . 'Login failed' . '<br>';
+                header("location:login.php");
+            }
     } else{
         $_SESSION['LoginError'] = $_SESSION['LoginError'] . 'Login failed' . '<br>';
         header("location:login.php");
