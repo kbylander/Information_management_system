db017a9ce6a0741c9ce0ca072364969afa5f24c5
kbylander <98012682+kbylander@users.noreply.github.com>
2022-03-01 16:57:10 +0100

    descriptions added to insertDB
    read above

--- Entryhandling/insertDB.php
+++ Entryhandling/insertDB.php
@@ -1,7 +1,19 @@
 <?php
 session_start();
+
+//Check so the user is logged in, and has access to the database
+if(!isset($_SESSION['loggedin'])) {
+    $_SESSION['loggedin'] = False;
+  }
+  if ($_SESSION['loggedin'] == False) {
+    header('Location: ../index.php');
+  }
+
+  
 $_SESSION['HeaderError']= '';
 
+
+//loops through each header
 foreach ($header as $key => $value){
     $priv = true;
     $entryIDend= strpos($value,'|');
@@ -10,12 +22,13 @@ foreach ($header as $key => $value){
     $firstchar=$value[0];
 
     include '../connectDB.php';
-
+    //checks if header contains all required elements
     if(($value[0] != '>') || (empty($entryIDend)) || (empty($genestart)) || (empty($speciesnamepos))){
         $_SESSION['HeaderError']= 'Header: ' . $value . ' is in incorrect format';
         header('Location:./insertform.php');    
     }
 
+    //creates atribute names from header.
     $entryid=substr($value,1,$entryIDend-1);
     $genename = substr($value, $genestart+5, $speciesnamepos-$genestart-5);
 
@@ -41,21 +54,23 @@ foreach ($header as $key => $value){
     $resnumberofrows = $row[0]+1;
     $seqid = $entryid . '_' . $genename .'_S' . $resnumberofrows;
 
-    echo $seqid;
-
     $sql_entry = "INSERT INTO entries (entryID, species, addedby) VALUES ('$entryid', '$species', '$currentuser')";
+    
     $sql_seq = "INSERT INTO sequence (seqID, genename, entryID, seq, seqaddedby) VALUES ('$seqid', '$genename', '$entryid', '$currentseq', '$currentuser')";
-
-
+   
+   //if entryID already exists, it will insert to sequence table, without creating a new record in the entries table. 
     if (!empty($results)){
         mysqli_query($link,$sql_seq);
     }
+    //if entryid doesn't exist, a new record will be created for both sequence and entries tables. 
     else{
         mysqli_query($link,$sql_entry);
         mysqli_query($link,$sql_seq);
     }
     include '../disconnectDB.php';
 }
+
+//redirect to sequences table of specific user.
 header('Location:../Database/sequencesDB.php');    
 
 ?>
